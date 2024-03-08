6c0bf5aea2c3c35c86894e3cb6d02786dd930fb3
lcrowther-snyk <lawrence.crowther@snyk.io>
2024-03-08 13:32:06 +1000

    put sql inline in homecontroller

--- src/main/java/org/workshop/coffee/controller/HomeController.java
+++ src/main/java/org/workshop/coffee/controller/HomeController.java
@@ -1,5 +1,6 @@
 package org.workshop.coffee.controller;
 
+import org.workshop.coffee.domain.Product;
 import org.workshop.coffee.repository.SearchRepository;
 import org.workshop.coffee.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +10,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import java.util.List;
+
 @Controller
 public class HomeController {
+    @Autowired
+    EntityManager em;
 
+    @Autowired
+    DataSource dataSource;
+    
     private ProductService productService;
-    private SearchRepository searchRepository;
 
     @Autowired
     public HomeController(ProductService productService, SearchRepository searchRepository) {
         this.productService = productService;
-        this.searchRepository = searchRepository;
     }
 
     @GetMapping({"/", "/index", "/home"})
@@ -29,7 +37,29 @@ public class HomeController {
 
     @PostMapping("/")
     public String searchProducts(Model model, @RequestParam String input) {
-        model.addAttribute("products", searchRepository.searchProduct(input));
+        model.addAttribute("products", searchProduct(input));
         return "index";
     }
+    public List<Product> searchProduct (String input) {
+       return null;
+    }
+
+
+
+
+
+
+
+
+
+
+//    public List<Product> searchProduct (String input) {
+//        //create sql query
+//        String sql = "SELECT * FROM product WHERE name LIKE '%" + input + "%'";
+//        //execute query
+//        return em.createNativeQuery(sql, Product.class).getResultList();
+//
+//    }
+
+
 }
